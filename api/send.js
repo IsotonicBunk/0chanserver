d23d342adc94a7259e92086d87bbad0127830775
isobun <115648600+IsotonicBunk@users.noreply.github.com>
2026-07-30 14:01:42 +0300

    Update send.js

--- api/send.js
+++ api/send.js
@@ -2,6 +2,25 @@ import { db } from "./firebase.js";
 import { Timestamp } from "firebase-admin/firestore";
 
 export default async function handler(req, res) {
+res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://isotonicbunk.github.io"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "POST, OPTIONS"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type"
+    );
+
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
 
     if (req.method !== "POST")
             return res.status(405).end();
