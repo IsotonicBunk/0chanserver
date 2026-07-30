e19da4d33282de205f58463b791ed0274384589d
isobun <115648600+IsotonicBunk@users.noreply.github.com>
2026-07-30 15:15:30 +0300

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,6 +1,8 @@
 import { db } from "./firebase.js";
 import { Timestamp } from "firebase-admin/firestore";
+import { inject } from "@vercel/analytics"
 
+inject()
 export default async function handler(req, res) {
 res.setHeader(
         "Access-Control-Allow-Origin",
